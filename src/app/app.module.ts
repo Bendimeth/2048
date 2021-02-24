f8f0388baf03242e6fdba724faf97c903001a761
Bendimeth <milosz.wodzisz@gmail.com>
2021-02-24 15:54:01 +0100

    Updated npm packages, added animations, overall code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {StoreService} from "./logic/store.service";
-import {MathService} from "./shared/math.service";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {StoreService} from './logic/store.service';
+import {MathService} from './shared/math.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
--- src/app/shared/math.service.ts
+++ src/app/shared/math.service.ts
@@ -1,11 +1,15 @@
 import {Injectable} from '@angular/core';
 
+import {GameStatus, IListValues, MoveTo} from '../entities/IStoreService';
+import {IMoveAction, IRandomNumber} from '../entities/IMathService';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MathService {
+  currentMoveScore = 0;
 
-  generateRandomNumbers(quantity: number, array: Array<number>) {
+  generateRandomNumbers(quantity: number, array: number[]): IRandomNumber[] {
     const filtered = array
       .map((el, index) => el === 0 ? index : -1)
       .filter((el) => el !== -1);
@@ -19,112 +23,106 @@ export class MathService {
     return randomNumbers;
   }
 
-  cutVertically(array, col) {
+  cutVertically(array: IListValues[], col: number): IListValues[] {
     const result = [];
-
     for (let i = col - 1; i < array.length; i += 4) {
       result.push(array[i]);
     }
     return result;
   }
 
-  compareArrays(first, second) {
-    let counter = 0;
-
-    first.map(
-      (el, idx) => {
-        if (el === second[idx]) {
-          counter++;
-        }
-      }
-    );
-
-    return counter === first.length ? 'stuck' : 'ok';
+  areArraysTheSame(a: number[], b: number[]): boolean {
+    return a.find((elem, index) => elem !== b[index]) === undefined;
   }
 
-  sortArray(array, leftOrRight: string) {
-    const initSorted = [];
-    let counter = 0;
-
-    if (leftOrRight === 'right' || leftOrRight === 'down') {
-      array = array.reverse();
-    }
-
-    array.map(
-      (el, idx, arr) => {
-        if (el !== 0) {
-          for (let i = idx; i < arr.length; i++) {
-            if (el === arr[i + 1]) {
-              initSorted.push(el + arr[i + 1]);
-              arr[i + 1] = 0;
-              break;
-            } else if (i + 1 >= arr.length) {
-              initSorted.push(el);
-            } else if (arr[i + 1]) {
-              initSorted.push(el);
-              break;
-            }
-          }
+  sortArray(array: IListValues[], direction: MoveTo): IListValues[] {
+    const listValues: IListValues[] = [];
+    let usedIndexes = 0;
+    const dataCopy = (direction === MoveTo.Bottom || direction === MoveTo.Right) ? array.slice().reverse() : array.slice();
+    dataCopy.filter(elem => elem.value > 0).forEach((elem, index, row) => {
+      if (usedIndexes <= index ) {
+        if (elem.value === row[index + 1]?.value) {
+          listValues.push({
+            updated: true,
+            moved: true,
+            direction,
+            value: elem.value * 2
+          });
+          this.currentMoveScore += elem.value * 3;
+          usedIndexes = index + 2;
         } else {
-          counter++;
+          listValues.push({
+            value: elem.value,
+            moved: dataCopy[index]?.value !== elem.value,
+            direction
+          });
+        }
+      }
+    });
+    for (let i = listValues.length; i < 4; i++) {
+      listValues.push({
+        value: 0,
+        destroyed: dataCopy[i]?.value > 0 && !listValues[i]?.value
+      });
+    }
+    return direction === MoveTo.Right || direction === MoveTo.Bottom ? listValues.reverse() : listValues;
+  }
+
+  isAnyMovePossible(array: number[]): boolean {
+    return array.filter((item, index) =>
+      (
+        (index !== 3 && index !== 7 && index !== 11 && array[index + 1] === item) ||
+        (array[index + 4] === item)
+      )).length !== 0;
+  }
+
+  matrixTranspositionOnFlatArray(listValues: IListValues[]): IListValues[] {
+    const towDimensionalArray: IListValues[][] = [[], [], [], []];
+    listValues.forEach(
+      (el, idx) => {
+        if ([0, 4, 8, 12].indexOf(idx) !== -1) {
+          towDimensionalArray[0].push(el);
+        } else if ([1, 5, 9, 13].indexOf(idx) !== -1) {
+          towDimensionalArray[1].push(el);
+        } else if ([2, 6, 10, 14].indexOf(idx) !== -1) {
+          towDimensionalArray[2].push(el);
+        } else if ([3, 7, 11, 15].indexOf(idx) !== -1) {
+          towDimensionalArray[3].push(el);
         }
       }
     );
-
-    for (let i = 0; i < counter; i++) {
-      initSorted.push(0);
-    }
-
-    return leftOrRight === 'right' || leftOrRight === 'down' ? initSorted.reverse() : initSorted;
+    return towDimensionalArray.reduce((result, row) => ([...result, ...row]), []);
   }
 
-  isAnyMovePossible(array: Array<number>): boolean {
-    return array.reduce((acc, el, idx, arr) => {
-      return acc || ((arr[idx + 1] !== undefined && arr[idx + 1] === el) || (arr[idx + 4] !== undefined && arr[idx + 4] === el));
-    }, false);
-  }
-
-  move(array, direction: string) {
-    if (direction === 'up' || direction === 'down') {
-
-      const col1 = this.sortArray(this.cutVertically(array, 1), direction);
-      const col2 = this.sortArray(this.cutVertically(array, 2), direction);
-      const col3 = this.sortArray(this.cutVertically(array, 3), direction);
-      const col4 = this.sortArray(this.cutVertically(array, 4), direction);
-
-      const colEnd1 = [];
-      const colEnd2 = [];
-      const colEnd3 = [];
-      const colEnd4 = [];
-
-      col1.concat(col2, col3, col4).map(
-        (el, idx) => {
-          if ([0, 4, 8, 12].indexOf(idx) !== -1) {
-            colEnd1.push(el);
-          } else if ([1, 5, 9, 13].indexOf(idx) !== -1) {
-            colEnd2.push(el);
-          } else if ([2, 6, 10, 14].indexOf(idx) !== -1) {
-            colEnd3.push(el);
-          } else if ([3, 7, 11, 15].indexOf(idx) !== -1) {
-            colEnd4.push(el);
-          }
-        }
-      );
+  move(array: IListValues[], direction: MoveTo): IMoveAction {
+    this.currentMoveScore = 0;
+    if (direction === MoveTo.Top || direction === MoveTo.Bottom) {
+      const combinedColumns = this.matrixTranspositionOnFlatArray([
+        ...this.sortArray(this.cutVertically(array, 1), direction),
+        ...this.sortArray(this.cutVertically(array, 2), direction),
+        ...this.sortArray(this.cutVertically(array, 3), direction),
+        ...this.sortArray(this.cutVertically(array, 4), direction)
+      ]);
+      const columnValues = combinedColumns.map(cell => cell.value);
 
       return {
-        list: colEnd1.concat(colEnd2, colEnd3, colEnd4),
-        status: this.compareArrays(colEnd1.concat(colEnd2, colEnd3, colEnd4), array) + direction
+        status: this.areArraysTheSame(columnValues, array.map(cell => cell.value)) ? GameStatus.Stuck : GameStatus.InProgress,
+        listValues: combinedColumns,
+        currentMoveScore: this.currentMoveScore
       };
-
     } else {
-      const row1 = this.sortArray(array.slice(0, 4), direction);
-      const row2 = this.sortArray(array.slice(4, 8), direction);
-      const row3 = this.sortArray(array.slice(8, 12), direction);
-      const row4 = this.sortArray(array.slice(12, 16), direction);
+      const combinedRows = [
+        ...this.sortArray(array.slice(0, 4), direction),
+        ...this.sortArray(array.slice(4, 8), direction),
+        ...this.sortArray(array.slice(8, 12), direction),
+        ...this.sortArray(array.slice(12, 16), direction)
+      ];
+      const rowsValues = combinedRows.map(row => row.value);
 
       return {
-        list: row1.concat(row2, row3, row4),
-        status: this.compareArrays(row1.concat(row2, row3, row4), array) + direction
+        status: this.areArraysTheSame(rowsValues, array.map(cell => cell.value)) ? GameStatus.Stuck : GameStatus.InProgress,
+        listValues: combinedRows,
+        currentMoveScore: this.currentMoveScore
       };
     }
   }
